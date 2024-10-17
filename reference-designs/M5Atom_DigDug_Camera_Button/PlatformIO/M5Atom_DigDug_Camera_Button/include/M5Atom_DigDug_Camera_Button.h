e966a9cd809199fc311d7a91fabd96ad026fb42c
David J Kordsmeier <dkords@gmail.com>
2024-10-17 16:14:52 -0700

    refs #74 #75 Add sample code for reference design, make it platform io

---
+++ reference-designs/M5Atom_DigDug_Camera_Button/PlatformIO/M5Atom_DigDug_Camera_Button/include/M5Atom_DigDug_Camera_Button.h
@@ -0,0 +1,8 @@
+#ifndef M5ATOM_DIGDUG_CAMERA_BUTTON_H
+#define M5ATOM_DIGDUG_CAMERA_BUTTON_H
+
+//============ Includes ====================
+#include "arduinoGlue.h"
+
+//============ Added by Convertor ==========
+#endif // M5ATOM_DIGDUG_CAMERA_BUTTON_H
---
+++ reference-designs/M5Atom_DigDug_Camera_Button/PlatformIO/M5Atom_DigDug_Camera_Button/include/arduinoGlue.h
@@ -0,0 +1,29 @@
+#ifndef ARDUINOGLUE_H
+#define ARDUINOGLUE_H
+
+
+//============ Includes ====================
+#include <ArduinoBLE.h>
+
+//============ Defines & Macros====================
+#define VERSION "0.0.4"
+#define LED_BUILTIN 13  // If your ESP32 DOES NOT DEFINE LED_BUILTIN
+
+//============ Structs, Unions & Enums ============
+//-- from M5Atom_DigDug_Camera_Button.ino
+enum ButtonState {
+  B_On,             // Camera Shooting
+  B_Off,            // Camera Not Shooting
+  B_Pwr             // Camera Off
+};
+
+//============ Extern Variables ============
+extern const int       BUTTON_PIN;                        		//-- from M5Atom_DigDug_Camera_Button
+extern BLEByteCharacteristic buttonCharacteristic;              		//-- from M5Atom_DigDug_Camera_Button
+extern int             currentState;                      		//-- from M5Atom_DigDug_Camera_Button
+extern ButtonState     g_btn_state;                       		//-- from M5Atom_DigDug_Camera_Button
+extern int             lastState;                         		//-- from M5Atom_DigDug_Camera_Button
+extern const int       ledPin;                            		//-- from M5Atom_DigDug_Camera_Button
+extern BLEService      ledService;                        		//-- from M5Atom_DigDug_Camera_Button
+
+#endif // ARDUINOGLUE_H
