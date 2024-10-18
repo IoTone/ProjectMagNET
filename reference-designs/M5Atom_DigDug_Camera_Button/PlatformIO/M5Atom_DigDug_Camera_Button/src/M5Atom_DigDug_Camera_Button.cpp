3d170f7114b7e4fc7e8dcd53602734f4451c70d8
David J Kordsmeier <dkords@gmail.com>
2024-10-17 17:02:17 -0700

    refs #75 small fix to re-advertise after a disconnect

--- reference-designs/M5Atom_DigDug_Camera_Button/PlatformIO/M5Atom_DigDug_Camera_Button/src/M5Atom_DigDug_Camera_Button.cpp
+++ reference-designs/M5Atom_DigDug_Camera_Button/PlatformIO/M5Atom_DigDug_Camera_Button/src/M5Atom_DigDug_Camera_Button.cpp
@@ -181,6 +181,7 @@ void loop() {
     // when the central disconnects, print it out:
     Serial.print(F("Disconnected from central: "));
     Serial.println(central.address());
+    BLE.advertise();
   }
 
   if (M5.Btn.wasPressed()) {
